59a8b1a93727ddf414de81e4d61c0cb9dd7ddd66
Xeler-ync <Xeler-ync@qq.com>
2025-11-02 01:55:40 +0800

    feat: add configuration management with config.json and example file

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import tornado.ioloop
 import tornado.autoreload
 from handlers.chat import ChatListHandler, ChatDetailHandler, ChatMessagesHandler
 from handlers.message import MessageHandler
+from config import config
 
 
 def make_app():
     # 配置日志等级
     logging.basicConfig(
-        level=logging.DEBUG,  # 可以是 DEBUG, INFO, WARNING, ERROR, CRITICAL
+        level=config.log_level,  # 可以是 DEBUG, INFO, WARNING, ERROR, CRITICAL
         # format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
 
     # 获取 Tornado 的 logger 并设置等级
-    tornado.log.access_log.setLevel(logging.DEBUG)
-    tornado.log.app_log.setLevel(logging.DEBUG)
-    tornado.log.gen_log.setLevel(logging.DEBUG)
+    tornado.log.access_log.setLevel(config.log_level)
+    tornado.log.app_log.setLevel(config.log_level)
+    tornado.log.gen_log.setLevel(config.log_level)
 
     return tornado.web.Application(
         [
@@ -31,7 +32,7 @@ def make_app():
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(54798, "0.0.0.0")
-    print("Server is running on http://localhost:54798")
+    app.listen(config.port, config.address)
+    print(f"Server is running on http://{config.address}:{config.port}")
     tornado.autoreload.start()  # 启动自动重载
     tornado.ioloop.IOLoop.current().start()
